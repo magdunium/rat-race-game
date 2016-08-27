944aad9fe24d62f3ab2a28d20ce70e7244d8e431
Magdum <magdunium@gmail.com>
2016-08-27 11:57:03 +0200

    Added: tilesprite, time and score set to 0

--- js/game.js
+++ js/game.js
@@ -1,25 +1,49 @@
-var rat, timeTextValue, gameTimeValue, ratAnim, level2Bckg;
+var rat, sanwich, timeTextValue, gameTimeValue, ratAnim, level2Bckg, score, scoreTextVal, textStyle, cursors, tilesprite;
 
 var Game = {
   preload: function() {
     game.load.spritesheet('rat', './assets/images/rat.png', 64, 64, 2);
-    game.load.image('leve2', './assets/images/bckg-level2.png');
+    game.load.spritesheet('sandwich', './assets/images/sandwich.png', 360, 360, 2);
+    game.load.image('level2', './assets/images/bckg-level2.png');
   },
 
   create: function(){
     // level2 background
-    this.add.image(0, 0, 'leve2');
+    tilesprite = game.add.tileSprite(0, 0, 800, 600, 'level2');
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+    //text style
+    textStyle = { font: "20px sans-serif", align: "center" };
 
     //rat animation
     rat = game.add.sprite(65, 440, 'rat', 2);
     rat.scale.set(1.5);
     ratAnim = rat.animations.add('walk');
+    ratAnim.play(4, true);
 
-    ratAnim.play(3, true);
+    //round timer
+    gameTimeValue = 0;
+    game.add.text(60, 30, 'Time: ', textStyle);
+    timeTextValue = game.add.text(150, 30, gameTimeValue.toString(), textStyle);
 
-    //time counter
-    timeTextValue = game.add.text(80, 50, 'Time: ');
-    timeTextValue.font = 'Sigmar One';
-    timeTextValue.fontSize = 20;
+    //score
+    score = 0;
+    game.add.text(60, 60, 'Score:', textStyle);
+    scoreTextVal = game.add.text(150, 60, score.toString(), textStyle);
+  },
+
+  update: function() {
+
+    if (cursors.left.isDown)
+    {
+        tilesprite.tilePosition.x += 8;
+    }
+    else if (cursors.right.isDown)
+    {
+        tilesprite.tilePosition.x -= 8;
+    }
+
   }
+
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var game;
 
-game = new Phaser.Game(800, 600, Phaser.auto, 'game');
+game = new Phaser.Game(800, 600, Phaser.auto, 'game', '');
 
 game.state.add('Menu', Menu);
 game.state.add('Game', Game);
